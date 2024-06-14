80bae10c1cb8c5ce4fdfe1f014b1c360c5a9effa
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-06-14 17:37:08 +0500

    zcvczv

---
+++ index.js
@@ -0,0 +1,55 @@
+// 1 - vazifa
+// let son1 = 13;
+// let son2 = 13;
+// console.log(son1 + son2)
+
+
+// 2 - vazifa
+// let a = '12';
+// let b = Number(a);
+// console.log(b);
+
+
+// 3 -vazifa
+// let taxminam = Math.floor(Math.random () * 100); 
+// if (taxminam % 2=== 0) {
+//     console.log(`${taxminam} -> juft son`);
+// }else {
+//     console.log (` ${random} -> toq son `)
+// }
+
+
+// 4 - vazifa
+// let soz = prompt("Ismingizni Kiriting");
+ 
+// if (soz === "Mars") {
+// }else {
+//     alert("Mars IT School");
+// }
+
+
+// 5 - vazifa
+// let letterSearch = ['non', 'asal', 'banan', 'olma', 'aziza', 'Gul', 'mashina',]
+// let katta5 = []
+// let kichik5 = []
+
+// for (let narsa of letterSearch) {
+//     if (narsa.length >= 5) {
+//         katta5.push(narsa)
+//     }else {
+//         kichik5.push (narsa); 
+//    }
+// }
+
+// console.log(katta5);
+// console.log(kichik5);
+
+
+// 6 - vazifa
+// for (let i =1; i <= 100; i++){
+//     if ( i % 2 === 0) {
+//         console.log (`1 ${i} -> juft son `)
+//     }else {
+//         console.log (`1 ${i} -> toq son `)
+//     }
+// }
